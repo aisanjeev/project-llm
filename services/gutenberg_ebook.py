eabe25003fe35311215ba7bba9fa5d88c91af022
Sanjeev Kumar <aisanjeevit@gmail.com>
2025-04-03 11:44:10 +0530

    updated logic

--- services/gutenberg_ebook.py
+++ services/gutenberg_ebook.py
@@ -184,19 +184,17 @@ def process_analysis(file_path, book_id):
         return data
     insert_ebook(book_id)
     chunk = chunk_text(text, max_length=5000)
-    print("chunk len", len(chunk))
     selected_chunks = select_chunks(chunk, num_samples=10)
-    print("selected_chunks len", len(selected_chunks))
     client=getGroqClient()
     summary = process_raw_analysis(client, selected_chunks)
     merged_output = merge_responses(summary)
     client=getGroqClient()
-    print("merged_output", merged_output)
     raw_json_output = process_final_analysis(merged_output, client)
-    print("raw_json_output", raw_json_output)
-    final_analysis = extract_json(raw_json_output)
-    print("final_analysis", final_analysis)
-    # print(final_analysis)
+    if type(raw_json_output ) == dict:
+        final_analysis = raw_json_output
+    else:
+        final_analysis = dict(json.loads(raw_json_output))
+    print(final_analysis)
     try:
         update_ebook_data(
             ebook_id=book_id,
